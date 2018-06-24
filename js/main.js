35eb71b7f93c88003321c7dab59254e9f234b9e2
TroyB <troy_budd95@msn.com>
2018-06-24 20:06:24 +0100

    Added API Keys to files

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoidHJveWI5NSIsImEiOiJjamltcmJtd2QwNmxxM3FxNzYxd3F5cGF4In0.jRKjG5m3dxLYqJVx7aFCfQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
